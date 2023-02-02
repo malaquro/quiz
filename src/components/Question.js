eb4b188545873fa5b6fa69f2876f0e98074e1c71
Nikita Zolotov <alfyorov.work@mail.ru>
2023-02-02 17:04:42 +0300

    create useState

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,11 +4,12 @@ const Question = () => {
   return (
     <div>
       <div className="question">Text of the question</div>
-      <div className="answers"></div>
-      <Answer />
-      <Answer />
-      <Answer />
-      <Answer />
+      <div className="answers">
+        <Answer />
+        <Answer />
+        <Answer />
+        <Answer />
+      </div>
     </div>
   );
 };
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Question from "./Question";
 
 const Quiz = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  console.log(currentQuestionIndex);
   return (
     <div className="quiz">
+      {currentQuestionIndex}
       <div>
         <div className="score">Question 1/8</div>
         <Question />
-        <div className="next-button">Next question</div>
+        <div
+          className="next-button"
+          onClick={() => setCurrentQuestionIndex(currentQuestionIndex + 1)}
+        >
+          Next question
+        </div>
       </div>
     </div>
   );
